f3b2f7b85ec88cbe6f5a00756d442f307eac1f5c
Ghoulbading <alex273dbg@gmail.com>
2025-12-10 10:37:51 +0500

    add search

--- src/api/apiNews.js
+++ src/api/apiNews.js
@@ -2,7 +2,12 @@ const BASE_URL = import.meta.env.VITE_NEWS_BASE_API_URL;
 const API_KEY = import.meta.env.VITE_NEWS_API_KEY;
 import axios from "axios";
 
-export async function getNews({ page_number = 1, page_size = 10, category }) {
+export async function getNews({
+  page_number = 1,
+  page_size = 10,
+  category,
+  keywords,
+}) {
   try {
     const response = await axios.get(`${BASE_URL}search`, {
       params: {
@@ -10,6 +15,7 @@ export async function getNews({ page_number = 1, page_size = 10, category }) {
         page_number,
         page_size,
         category,
+        keywords,
       },
     });
     return response.data;
