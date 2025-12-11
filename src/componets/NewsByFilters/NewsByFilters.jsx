2cc8b8f5211b83a749602cf372522bd26ee042c5
Ghoulbading <alex273dbg@gmail.com>
2025-12-11 10:34:16 +0500

    add lattest news

--- src/componets/NewsByFilters/NewsByFilters.jsx
+++ src/componets/NewsByFilters/NewsByFilters.jsx
@@ -1,15 +1,28 @@
-import { TOTAL_PAGES } from "../../constants/constants";
+import { getNews } from "../../api/apiNews";
+import { PAGE_SIZE, TOTAL_PAGES } from "../../constants/constants";
+import { useDebounce } from "../../helpers/hooks/useDebounce";
+import { useFetch } from "../../helpers/hooks/useFetch";
+import { useFilter } from "../../helpers/hooks/useFilter";
 import NewsFilters from "../NewsFilters/NewsFilters";
 import NewsList from "../NewsList/NewsList";
-import Pagination from "../Pagination/Pagination";
+import PaginationWrtapper from "../PaginationWrtapper/PaginationWrtapper";
 import styles from "./styles.module.css";
 
-export default function NewsByFilters({
-  filter,
-  chengeFilter,
-  isLoading,
-  news,
-}) {
+export default function NewsByFilters() {
+  const { filter, chengeFilter } = useFilter({
+    page_number: 1,
+    page_size: PAGE_SIZE,
+    category: null,
+    keywords: "",
+  });
+
+  const debouncedKeywords = useDebounce(filter.keywords, 1500);
+
+  const { data, isLoading } = useFetch(getNews, {
+    ...filter,
+    keywords: debouncedKeywords,
+  });
+
   function heandleNextPage() {
     if (filter.page_number < TOTAL_PAGES) {
       chengeFilter("page_number", filter.page_number + 1);
@@ -30,23 +43,17 @@ export default function NewsByFilters({
     <section className={styles.section}>
       <NewsFilters filter={filter} chengeFilter={chengeFilter} />
 
-      <Pagination
+      <PaginationWrtapper
         totalPages={TOTAL_PAGES}
         heandleNextPage={heandleNextPage}
         heandlePageClick={heandlePageClick}
         heandlePrevPage={heandlePrevPage}
         currentPage={filter.page_number}
-      />
-
-      <NewsList news={news} isLoading={isLoading} />
-
-      <Pagination
-        totalPages={TOTAL_PAGES}
-        heandleNextPage={heandleNextPage}
-        heandlePageClick={heandlePageClick}
-        heandlePrevPage={heandlePrevPage}
-        currentPage={filter.page_number}
-      />
+        top
+        bottom
+      >
+        <NewsList news={data?.news} isLoading={isLoading} />
+      </PaginationWrtapper>
     </section>
   );
 }
--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,36 +1,13 @@
 import styles from "./styles.module.css";
-import { getNews } from "../../api/apiNews.js";
-import { useDebounce } from "../../helpers/hooks/useDebounce.js";
-import { PAGE_SIZE } from "../../constants/constants.js";
-import { useFetch } from "../../helpers/hooks/useFetch.js";
-import { useFilter } from "../../helpers/hooks/useFilter.js";
 import LatestNews from "../../componets/LatestNews/LatestNews.jsx";
 import NewsByFilters from "../../componets/NewsByFilters/NewsByFilters.jsx";
 
 export default function Main() {
-  const { filter, chengeFilter } = useFilter({
-    page_number: 1,
-    page_size: PAGE_SIZE,
-    category: null,
-    keywords: "",
-  });
-
-  const debouncedKeywords = useDebounce(filter.keywords, 1500);
-  const { data, isLoading } = useFetch(getNews, {
-    ...filter,
-    keywords: debouncedKeywords,
-  });
-
   return (
     <main className={styles.main}>
-      <LatestNews banners={data && data.news} isLoading={isLoading} />
+      <LatestNews />
 
-      <NewsByFilters
-        news={data?.news}
-        isLoading={isLoading}
-        filter={filter}
-        chengeFilter={chengeFilter}
-      />
+      <NewsByFilters />
     </main>
   );
 }
