baac28f04e436582ab66ffad5844710f86207eed
Ghoulbading <alex273dbg@gmail.com>
2026-02-19 10:19:37 +0500

    FSD

---
+++ src/entities/news/index.ts
@@ -0,0 +1,5 @@
+import type { INews, NewsApiResponse } from "./model/types";
+import NewsBanner from "./ui/NewsBanner/NewsBanner";
+import NewsItem from "./ui/NewsItem/NewsItem";
+
+export { INews, NewsApiResponse, NewsBanner, NewsItem };
